d76eb56f12b6b1fdb8f4a51d4cd2364877a01406
waffi._ <wafiq3040@gmail.com>
2024-11-13 16:25:52 +0700

    Delete backend directory

--- backend/prisma/migrations/20241109033920_5/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to drop the column `sku` on the `Product` table. All the data in the column will be lost.
-
-*/
--- AlterTable
-ALTER TABLE `Product` DROP COLUMN `sku`;
--- backend/src/common/interfaces/user.ts
+++
@@ -1,9 +0,0 @@
-import { User } from "@prisma/client"
-
-export type UserData = Omit<User, 'password'>;
-export interface AuthenticatedRequest extends Request {
-  user: {
-    sub: string;
-    email: string;
-  };
-}
